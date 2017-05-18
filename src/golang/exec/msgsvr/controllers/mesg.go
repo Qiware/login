19af9ac7f68952e2c22908fcc15922d2fbc28c7a
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2017-05-18 16:56:29 +0800

    [MSGSVR]: 添加事件统计的信息处理

--- src/golang/exec/msgsvr/controllers/mesg.go
+++ src/golang/exec/msgsvr/controllers/mesg.go
@@ -274,6 +274,462 @@ func MsgSvrBrowserEnvHandler(cmd uint32, nid uint32, data []byte, length uint32,
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// 事件统计
+
+/******************************************************************************
+ **函数名称: event_statistic_parse
+ **功    能: 事件统计信息
+ **输入参数:
+ **     data: 接收的数据
+ **输出参数: NONE
+ **返    回:
+ **     head: 通用协议头
+ **     req: 协议体内容
+ **     code: 错误码
+ **     err: 错误描述
+ **实现描述:
+ **注意事项:
+ **作    者: # Qifeng.zou # 2016.10.30 22:32:23 #
+ ******************************************************************************/
+func (ctx *MsgSvrCntx) event_statistic_parse(data []byte) (
+	head *comm.MesgHeader, req *mesg.MesgEventStatistic, code uint32, err error) {
+	/* > 字节序转换 */
+	head = comm.MesgHeadNtoh(data)
+	if !head.IsValid(1) {
+		errmsg := "Event-statistic head is invalid!"
+		ctx.log.Error("Header is invalid! cmd:0x%04X nid:%d",
+			head.GetCmd(), head.GetNid())
+		return nil, nil, comm.ERR_SVR_HEAD_INVALID, errors.New(errmsg)
+	}
+
+	ctx.log.Debug("Event-statistic header! cmd:0x%04X length:%d sid:%d cid:%d nid:%d seq:%d",
+		head.GetCmd(), head.GetLength(),
+		head.GetSid(), head.GetCid(), head.GetNid(), head.GetSeq())
+
+	/* > 解析PB协议 */
+	req = &mesg.MesgEventStatistic{}
+
+	err = proto.Unmarshal(data[comm.MESG_HEAD_SIZE:], req)
+	if nil != err {
+		ctx.log.Error("Unmarshal body failed! errmsg:%s", err.Error())
+		return head, nil, comm.ERR_SVR_BODY_INVALID, errors.New("Unmarshal body failed!")
+	}
+
+	return head, req, 0, nil
+}
+
+/******************************************************************************
+ **函数名称: event_statistic_failed
+ **功    能: 事件统计失败
+ **输入参数:
+ **     head: 协议头
+ **     req: 事件统计
+ **     code: 错误码
+ **     errmsg: 错误描述
+ **输出参数: NONE
+ **返    回: VOID
+ **实现描述:
+ **应答协议:
+ ** {
+ **     optional uint32 code = 1;       // M|错误码|数字|
+ **     optional string errmsg = 2;     // M|错误描述|字串|
+ ** }
+ **注意事项:
+ **作    者: # Qifeng.zou # 2017.05.18 10:02:34 #
+ ******************************************************************************/
+func (ctx *MsgSvrCntx) event_statistic_failed(head *comm.MesgHeader,
+	req *mesg.MesgEventStatistic, code uint32, errmsg string) int {
+	if nil == head {
+		return -1
+	}
+
+	/* > 设置协议体 */
+	ack := &mesg.MesgEventStatisticAck{
+		Code:   proto.Uint32(code),
+		Errmsg: proto.String(errmsg),
+	}
+
+	/* 生成PB数据 */
+	body, err := proto.Marshal(ack)
+	if nil != err {
+		ctx.log.Error("Marshal protobuf failed! errmsg:%s", err.Error())
+		return -1
+	}
+
+	length := len(body)
+
+	/* > 拼接协议包 */
+	p := &comm.MesgPacket{}
+	p.Buff = make([]byte, comm.MESG_HEAD_SIZE+length)
+
+	head.Cmd = comm.CMD_EVENT_STATISTIC_ACK
+	head.Length = uint32(length)
+
+	comm.MesgHeadHton(head, p)
+	copy(p.Buff[comm.MESG_HEAD_SIZE:], body)
+
+	/* > 发送协议包 */
+	ctx.frwder.AsyncSend(comm.CMD_EVENT_STATISTIC_ACK, p.Buff, uint32(len(p.Buff)))
+
+	ctx.log.Debug("Send event-statistic ack succ!")
+
+	return 0
+}
+
+/******************************************************************************
+ **函数名称: event_statistic_ack
+ **功    能: 事件统计应答
+ **输入参数:
+ **输出参数: NONE
+ **返    回: VOID
+ **实现描述:
+ ** {
+ **     optional uint32 code = 2;       // M|错误码|数字|
+ **     optional string errmsg = 3;     // M|错误描述|字串|
+ ** }
+ **注意事项:
+ **作    者: # Qifeng.zou # 2017.05.18 16:32:35 #
+ ******************************************************************************/
+func (ctx *MsgSvrCntx) event_statistic_ack(head *comm.MesgHeader, req *mesg.MesgBrowserEnv) int {
+	/* > 设置协议体 */
+	ack := &mesg.MesgEventStatisticAck{
+		Code:   proto.Uint32(0),
+		Errmsg: proto.String("Ok"),
+	}
+
+	/* 生成PB数据 */
+	body, err := proto.Marshal(ack)
+	if nil != err {
+		ctx.log.Error("Marshal protobuf failed! errmsg:%s", err.Error())
+		return -1
+	}
+
+	length := len(body)
+
+	/* > 拼接协议包 */
+	p := &comm.MesgPacket{}
+	p.Buff = make([]byte, comm.MESG_HEAD_SIZE+length)
+
+	head.Cmd = comm.CMD_EVENT_STATISTIC_ACK
+	head.Length = uint32(length)
+
+	comm.MesgHeadHton(head, p)
+	copy(p.Buff[comm.MESG_HEAD_SIZE:], body)
+
+	/* > 发送协议包 */
+	ctx.frwder.AsyncSend(comm.CMD_EVENT_STATISTIC_ACK, p.Buff, uint32(len(p.Buff)))
+
+	ctx.log.Debug("Send event-statistic ack success!")
+
+	return 0
+}
+
+/******************************************************************************
+ **函数名称: event_statistic_handler
+ **功    能: 事件统计处理
+ **输入参数:
+ **     head: 协议头
+ **     req: 事件统计信息
+ **输出参数: NONE
+ **返    回: 异常信息
+ **实现描述:
+ **注意事项:
+ **作    者: # Qifeng.zou # 2017.05.18 16:33:41 #
+ ******************************************************************************/
+func (ctx *MsgSvrCntx) event_statistic_handler(
+	head *comm.MesgHeader, req *mesg.MesgEventStatistic) (err error) {
+	pl := ctx.redis.Get()
+	defer func() {
+		pl.Do("")
+		pl.Close()
+	}()
+
+	ttl := time.Now().Unix() + comm.AE_SID_TTL
+
+	/* 记录SID集合 */
+	pl.Send("ZADD", comm.AE_KEY_SID_ZSET, ttl, head.GetSid())
+
+	/* > 插件信息 */
+	key := fmt.Sprintf(comm.AE_KEY_SID_STATISTIC, head.GetSid())
+
+	switch req.GetCtrl() {
+	case comm.CTL_IBX_USR: // 输入框: 用户名
+		switch req.GetEvent() {
+		case comm.EV_CHANGE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_CHANGE, req.GetCount())
+		case comm.EV_CLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_CLICK, req.GetCount())
+		case comm.EV_DBLCLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_DBLCLICK, req.GetCount())
+		case comm.EV_FOCUS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_FOCUS, req.GetCount())
+		case comm.EV_KEYDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_KEYDOWN, req.GetCount())
+		case comm.EV_KEYPRESS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_KEYPRESS, req.GetCount())
+		case comm.EV_KEYUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_KEYUP, req.GetCount())
+		case comm.EV_MOUSEDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_MOUSEDOWN, req.GetCount())
+		case comm.EV_MOUSEMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_MOUSEMOVE, req.GetCount())
+		case comm.EV_MOUSEOUT:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_MOUSEOUT, req.GetCount())
+		case comm.EV_MOUSEOVER:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_MOUSEOVER, req.GetCount())
+		case comm.EV_MOUSEUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_MOUSEUP, req.GetCount())
+		case comm.EV_TOUCHSTART:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_TOUCHSTART, req.GetCount())
+		case comm.EV_TOUCHMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_TOUCHMOVE, req.GetCount())
+		case comm.EV_TOUCHEND:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_TOUCHEND, req.GetCount())
+		case comm.EV_TOUCHCANCEL:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_USR_TOUCHCANCEL, req.GetCount())
+		}
+	case comm.CTL_IBX_PWD: // 输入框: 密码
+		switch req.GetEvent() {
+		case comm.EV_CHANGE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_CHANGE, req.GetCount())
+		case comm.EV_CLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_CLICK, req.GetCount())
+		case comm.EV_DBLCLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_DBLCLICK, req.GetCount())
+		case comm.EV_FOCUS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_FOCUS, req.GetCount())
+		case comm.EV_KEYDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_KEYDOWN, req.GetCount())
+		case comm.EV_KEYPRESS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_KEYPRESS, req.GetCount())
+		case comm.EV_KEYUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_KEYUP, req.GetCount())
+		case comm.EV_MOUSEDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_MOUSEDOWN, req.GetCount())
+		case comm.EV_MOUSEMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_MOUSEMOVE, req.GetCount())
+		case comm.EV_MOUSEOUT:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_MOUSEOUT, req.GetCount())
+		case comm.EV_MOUSEOVER:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_MOUSEOVER, req.GetCount())
+		case comm.EV_MOUSEUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_MOUSEUP, req.GetCount())
+		case comm.EV_TOUCHSTART:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_TOUCHSTART, req.GetCount())
+		case comm.EV_TOUCHMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_TOUCHMOVE, req.GetCount())
+		case comm.EV_TOUCHEND:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_TOUCHEND, req.GetCount())
+		case comm.EV_TOUCHCANCEL:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_PWD_TOUCHCANCEL, req.GetCount())
+		}
+	case comm.CTL_IBX_IMG: // 输入框: 图片验证码
+		switch req.GetEvent() {
+		case comm.EV_CHANGE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_CHANGE, req.GetCount())
+		case comm.EV_CLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_CLICK, req.GetCount())
+		case comm.EV_DBLCLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_DBLCLICK, req.GetCount())
+		case comm.EV_FOCUS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_FOCUS, req.GetCount())
+		case comm.EV_KEYDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_KEYDOWN, req.GetCount())
+		case comm.EV_KEYPRESS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_KEYPRESS, req.GetCount())
+		case comm.EV_KEYUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_KEYUP, req.GetCount())
+		case comm.EV_MOUSEDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_MOUSEDOWN, req.GetCount())
+		case comm.EV_MOUSEMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_MOUSEMOVE, req.GetCount())
+		case comm.EV_MOUSEOUT:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_MOUSEOUT, req.GetCount())
+		case comm.EV_MOUSEOVER:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_MOUSEOVER, req.GetCount())
+		case comm.EV_MOUSEUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_MOUSEUP, req.GetCount())
+		case comm.EV_TOUCHSTART:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_TOUCHSTART, req.GetCount())
+		case comm.EV_TOUCHMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_TOUCHMOVE, req.GetCount())
+		case comm.EV_TOUCHEND:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_TOUCHEND, req.GetCount())
+		case comm.EV_TOUCHCANCEL:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_IMG_TOUCHCANCEL, req.GetCount())
+		}
+	case comm.CTL_BTN_IMG: // 按钮: 图片验证码
+		switch req.GetEvent() {
+		case comm.EV_CLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_IMG_CLICK, req.GetCount())
+		case comm.EV_DBLCLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_IMG_DBLCLICK, req.GetCount())
+		case comm.EV_FOCUS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_IMG_FOCUS, req.GetCount())
+		case comm.EV_KEYDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_IMG_KEYDOWN, req.GetCount())
+		case comm.EV_KEYPRESS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_IMG_KEYPRESS, req.GetCount())
+		case comm.EV_KEYUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_IMG_KEYUP, req.GetCount())
+		case comm.EV_MOUSEDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_IMG_MOUSEDOWN, req.GetCount())
+		case comm.EV_MOUSEMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_IMG_MOUSEMOVE, req.GetCount())
+		case comm.EV_MOUSEOUT:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_IMG_MOUSEOUT, req.GetCount())
+		case comm.EV_MOUSEOVER:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_IMG_MOUSEOVER, req.GetCount())
+		case comm.EV_MOUSEUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_IMG_MOUSEUP, req.GetCount())
+		case comm.EV_TOUCHSTART:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_IMG_TOUCHSTART, req.GetCount())
+		case comm.EV_TOUCHMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_IMG_TOUCHMOVE, req.GetCount())
+		case comm.EV_TOUCHEND:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_IMG_TOUCHEND, req.GetCount())
+		case comm.EV_TOUCHCANCEL:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_IMG_TOUCHCANCEL, req.GetCount())
+		}
+	case comm.CTL_IBX_TEL: // 输入框: 手机号
+		switch req.GetEvent() {
+		case comm.EV_CHANGE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_CHANGE, req.GetCount())
+		case comm.EV_CLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_CLICK, req.GetCount())
+		case comm.EV_DBLCLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_DBLCLICK, req.GetCount())
+		case comm.EV_FOCUS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_FOCUS, req.GetCount())
+		case comm.EV_KEYDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_KEYDOWN, req.GetCount())
+		case comm.EV_KEYPRESS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_KEYPRESS, req.GetCount())
+		case comm.EV_KEYUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_KEYUP, req.GetCount())
+		case comm.EV_MOUSEDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_MOUSEDOWN, req.GetCount())
+		case comm.EV_MOUSEMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_MOUSEMOVE, req.GetCount())
+		case comm.EV_MOUSEOUT:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_MOUSEOUT, req.GetCount())
+		case comm.EV_MOUSEOVER:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_MOUSEOVER, req.GetCount())
+		case comm.EV_MOUSEUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_MOUSEUP, req.GetCount())
+		case comm.EV_TOUCHSTART:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_TOUCHSTART, req.GetCount())
+		case comm.EV_TOUCHMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_TOUCHMOVE, req.GetCount())
+		case comm.EV_TOUCHEND:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_TOUCHEND, req.GetCount())
+		case comm.EV_TOUCHCANCEL:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_TEL_TOUCHCANCEL, req.GetCount())
+		}
+	case comm.CTL_IBX_SMS: // 输入框: 短信验证码
+		switch req.GetEvent() {
+		case comm.EV_CHANGE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_CHANGE, req.GetCount())
+		case comm.EV_CLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_CLICK, req.GetCount())
+		case comm.EV_DBLCLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_DBLCLICK, req.GetCount())
+		case comm.EV_FOCUS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_FOCUS, req.GetCount())
+		case comm.EV_KEYDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_KEYDOWN, req.GetCount())
+		case comm.EV_KEYPRESS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_KEYPRESS, req.GetCount())
+		case comm.EV_KEYUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_KEYUP, req.GetCount())
+		case comm.EV_MOUSEDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_MOUSEDOWN, req.GetCount())
+		case comm.EV_MOUSEMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_MOUSEMOVE, req.GetCount())
+		case comm.EV_MOUSEOUT:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_MOUSEOUT, req.GetCount())
+		case comm.EV_MOUSEOVER:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_MOUSEOVER, req.GetCount())
+		case comm.EV_MOUSEUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_MOUSEUP, req.GetCount())
+		case comm.EV_TOUCHSTART:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_TOUCHSTART, req.GetCount())
+		case comm.EV_TOUCHMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_TOUCHMOVE, req.GetCount())
+		case comm.EV_TOUCHEND:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_TOUCHEND, req.GetCount())
+		case comm.EV_TOUCHCANCEL:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_IBX_SMS_TOUCHCANCEL, req.GetCount())
+		}
+	case comm.CTL_BTN_SMS: // 按钮: 短信验证码
+		switch req.GetEvent() {
+		case comm.EV_CLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_SMS_CLICK, req.GetCount())
+		case comm.EV_DBLCLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_SMS_DBLCLICK, req.GetCount())
+		case comm.EV_FOCUS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_SMS_FOCUS, req.GetCount())
+		case comm.EV_KEYDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_SMS_KEYDOWN, req.GetCount())
+		case comm.EV_KEYPRESS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_SMS_KEYPRESS, req.GetCount())
+		case comm.EV_KEYUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_SMS_KEYUP, req.GetCount())
+		case comm.EV_MOUSEDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_SMS_MOUSEDOWN, req.GetCount())
+		case comm.EV_MOUSEMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_SMS_MOUSEMOVE, req.GetCount())
+		case comm.EV_MOUSEOUT:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_SMS_MOUSEOUT, req.GetCount())
+		case comm.EV_MOUSEOVER:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_SMS_MOUSEOVER, req.GetCount())
+		case comm.EV_MOUSEUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_SMS_MOUSEUP, req.GetCount())
+		case comm.EV_TOUCHSTART:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_SMS_TOUCHSTART, req.GetCount())
+		case comm.EV_TOUCHMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_SMS_TOUCHMOVE, req.GetCount())
+		case comm.EV_TOUCHEND:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_SMS_TOUCHEND, req.GetCount())
+		case comm.EV_TOUCHCANCEL:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_SMS_TOUCHCANCEL, req.GetCount())
+		}
+	case comm.CTL_BTN_LGN: // 按钮: 登录
+		switch req.GetEvent() {
+		case comm.EV_CLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_LGN_CLICK, req.GetCount())
+		case comm.EV_DBLCLICK:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_LGN_DBLCLICK, req.GetCount())
+		case comm.EV_FOCUS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_LGN_FOCUS, req.GetCount())
+		case comm.EV_KEYDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_LGN_KEYDOWN, req.GetCount())
+		case comm.EV_KEYPRESS:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_LGN_KEYPRESS, req.GetCount())
+		case comm.EV_KEYUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_LGN_KEYUP, req.GetCount())
+		case comm.EV_MOUSEDOWN:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_LGN_MOUSEDOWN, req.GetCount())
+		case comm.EV_MOUSEMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_LGN_MOUSEMOVE, req.GetCount())
+		case comm.EV_MOUSEOUT:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_LGN_MOUSEOUT, req.GetCount())
+		case comm.EV_MOUSEOVER:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_LGN_MOUSEOVER, req.GetCount())
+		case comm.EV_MOUSEUP:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_LGN_MOUSEUP, req.GetCount())
+		case comm.EV_TOUCHSTART:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_LGN_TOUCHSTART, req.GetCount())
+		case comm.EV_TOUCHMOVE:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_LGN_TOUCHMOVE, req.GetCount())
+		case comm.EV_TOUCHEND:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_LGN_TOUCHEND, req.GetCount())
+		case comm.EV_TOUCHCANCEL:
+			pl.Send("HINCRBY", key, comm.COL_EVENT_BTN_LGN_TOUCHCANCEL, req.GetCount())
+		}
+	}
+	return err
+}
 
 /******************************************************************************
  **函数名称: MsgSvrEventStatisticHandler
