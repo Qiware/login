f10059d46e039a71e62483e52ece625e39022a79
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2017-05-23 10:36:34 +0800

    [USRSVR]: 支持CORS跨域支持

--- src/golang/exec/usrsvr/main.go
+++ src/golang/exec/usrsvr/main.go
@@ -6,6 +6,7 @@ import (
 	"runtime"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 
 	"ai-eye/src/golang/exec/usrsvr/controllers"
 	"ai-eye/src/golang/exec/usrsvr/controllers/conf"
@@ -73,6 +74,13 @@ func register(ctx *controllers.UsrSvrCntx) {
 /* 启动服务 */
 func launch(ctx *controllers.UsrSvrCntx) {
 	ctx.Launch()
+
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:    []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Content-Type"},
+		ExposeHeaders:   []string{"Content-Length", "Access-Control-Allow-Origin"},
+	}))
 	beego.Run()
 }
 
