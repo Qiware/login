0dd8d69b1b34a3321a2d1e9bd6095f36301a7433
Qifeng.zou <Qifeng.zou.job@hotmail.com>
2017-05-23 14:52:59 +0800

    [MESG]: 纠正消息定义列表

--- src/clang/incl/cmd_list.h
+++ src/clang/incl/cmd_list.h
@@ -15,30 +15,30 @@ typedef enum
     CMD_UNKNOWN                             /* 未知消息 */
 
     /* 通用消息 */
-    , CMD_ONLINE                = 0x0001    /* 上线请求(服务端) */
-    , CMD_ONLINE_ACK            = 0x0002    /* 上线请求应答(客户端) */
+    , CMD_ONLINE                = 0x0101    /* 上线请求(服务端) */
+    , CMD_ONLINE_ACK            = 0x0102    /* 上线请求应答(客户端) */
 
-    , CMD_OFFLINE               = 0x0003    /* 下线请求(服务端) */
-    , CMD_OFFLINE_ACK           = 0x0004    /* 下线请求应答(客户端) */
+    , CMD_OFFLINE               = 0x0103    /* 下线请求(服务端) */
+    , CMD_OFFLINE_ACK           = 0x0104    /* 下线请求应答(客户端) */
 
-    , CMD_PING                  = 0x0005    /* 客户端心跳(服务端) */
-    , CMD_PONG                  = 0x0006    /* 客户端心跳应答(客户端) */
+    , CMD_PING                  = 0x0105    /* 客户端心跳(服务端) */
+    , CMD_PONG                  = 0x0106    /* 客户端心跳应答(客户端) */
 
-    , CMD_KICK                  = 0x0007    /* 踢某连接下线 */
-    , CMD_KICK_ACK              = 0x0008    /* 踢某连接下线应答 */
+    , CMD_KICK                  = 0x0107    /* 踢某连接下线 */
+    , CMD_KICK_ACK              = 0x0108    /* 踢某连接下线应答 */
 
     /* 数据上报 */
-    , CMD_BROWSER_ENV           = 0x0101    /* 环境信息 */
-    , CMD_BROWSER_ENV_ACK       = 0x0102    /* 环境信息应答 */
-    , CMD_EVENT_STATISTIC       = 0x0103    /* 事件统计 */
-    , CMD_EVENT_STATISTIC_ACK   = 0x0104    /* 事件统计应答 */
+    , CMD_BROWSER_ENV           = 0x0201    /* 环境信息 */
+    , CMD_BROWSER_ENV_ACK       = 0x0202    /* 环境信息应答 */
+    , CMD_EVENT_STATISTIC       = 0x0203    /* 事件统计 */
+    , CMD_EVENT_STATISTIC_ACK   = 0x0204    /* 事件统计应答 */
 
     /* 系统内部消息 */
-    , CMD_LSND_INFO             = 0x0601    /* 帧听层信息上报 */
-    , CMD_LSND_INFO_ACK         = 0x0602    /* 帧听层信息上报应答 */
+    , CMD_LSND_INFO             = 0x0301    /* 帧听层信息上报 */
+    , CMD_LSND_INFO_ACK         = 0x0302    /* 帧听层信息上报应答 */
 
-    , CMD_FRWD_INFO             = 0x0603    /* 转发层信息上报 */
-    , CMD_FRWD_INFO_ACK         = 0x0604    /* 转发层信息上报应答 */
+    , CMD_FRWD_INFO             = 0x0303    /* 转发层信息上报 */
+    , CMD_FRWD_INFO_ACK         = 0x0304    /* 转发层信息上报应答 */
 } mesg_type_e;
 
 #endif /*__CMD_LIST_H__*/
